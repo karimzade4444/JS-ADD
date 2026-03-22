0ffa37e1e187ae6f5133b3b32689e87cfeee23d2
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-03-22 18:05:25 +0500

    JS selector

---
+++ script.js
@@ -0,0 +1,7 @@
+let header = document.querySelector(".Header")
+let top = document.querySelector(".top")
+let middle = document.querySelector(".middle")
+let add = document.querySelector(".add")
+let Search = document.querySelector(".Search")
+
+
