a1d8f15fffafa70257ccd8f151e2f46fc6575fea
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-03-22 20:40:39 +0500

    View

--- scr.js
+++ scr.js
@@ -5,23 +5,23 @@ let middle = document.querySelector(".middle");
 let add = document.querySelector(".add");
 let Search = document.querySelector(".Search");
 let backcreatmodal = document.querySelector(".backcreatmodal");
-let closes = document.querySelector(".close")
-let cancel = document.querySelector(".Cancel")
-let creatmodaltop = document.querySelector(".creatmodaltop")
+let closes = document.querySelector(".close");
+let cancel = document.querySelector(".Cancel");
+let creatmodaltop = document.querySelector(".creatmodaltop");
+let edit = document.querySelector(".edit");
+let view = document.querySelector(".view");
+let newBL= document.querySelector(".new")
 
-add.onclick=()=>{
-backcreatmodal.style.display = "block";
-}
+add.onclick = () => {
+  backcreatmodal.style.display = "block";
+};
 
-const closemodal = ()=>{
-    backcreatmodal.style.display = "none"
-}
+const closemodal = () => {
+  backcreatmodal.style.display = "none";
+};
 
-closes.onclick = closemodal
-cancel.onclick = closemodal
-
-
-
+closes.onclick = closemodal;
+cancel.onclick = closemodal;
 
 
 
