a8d555d786916cdb492d05a8ee95e7600b0ae176
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-03-22 18:54:42 +0500

    Modal

--- script.js
+++ script.js
@@ -4,25 +4,4 @@ let middle = document.querySelector(".middle")
 let add = document.querySelector(".add")
 let Search = document.querySelector(".Search")
 
-function render() {
-  container.innerHTML = "";
-  middle.forEach((el) => {
-    let div = document.createElement("div");
-    div.classList.add("card");
-    div.style.backgroundColor = el.color;
-    const pName = document.createElement("p");
-    pName.textContent = el.name;
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "X";
-    deleteButton.classList.add("deleteButton");
-    deleteButton.onclick = () => {
-      handleDeleteFruit(el.id);
-    };
-    const editButton = document.createElement("button");
-    editButton.textContent = "😕";
-    editButton.onclick = () => {
-      editModal.style.display = "block";
-      editColorInput.value = el.color;
-      editNameInput.value = el.name;
-      idModal.value = el.id;
-    }})};
+
