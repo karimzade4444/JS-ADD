633e6bf117b27253699c919508aa865786393570
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-03-23 17:08:43 +0500

    JS OBJ Block

--- scr.js
+++ scr.js
@@ -10,12 +10,39 @@ let cancel = document.querySelector(".Cancel");
 let creatmodaltop = document.querySelector(".creatmodaltop");
 let edit = document.querySelector(".edit");
 let view = document.querySelector(".view");
-let newBL= document.querySelector(".new")
 
 
 let blocks = [
-  {id:1, img: "https://i.pinimg.com/736x/06/14/fa/0614fa155de10e2072149b780c3e2e60.jpg",parag:"Tajikistan", text: "Feel the friendship"}
+  {id:1, img: "https://i.pinimg.com/736x/06/14/fa/0614fa155de10e2072149b780c3e2e60.jpg",par:"Tajikistan", txt: "Feel the friendship"}
 ]
+
+function render(data){
+data.forEach((el)=>{
+  let newss = document.createElement("div");
+   newss.classList.add("newss")
+   middle.append(newss)
+  let img = document.createElement("div");
+  img.classList.add("img")
+  newss.append(img)
+  let images = document.createElement("img")
+  images.classList.add("images")
+  images.src = el.img;
+  img.append(images)
+  let about = document.createElement("div")
+  about.classList.add("about")
+  newss.append(about)
+  let parag = document.createElement("h1")
+  parag.classList.add("parag")
+  parag.textContent = el.par;
+  about.append(parag)
+  let text = 
+
+
+
+});
+
+}
+
 
 
 add.onclick = () => {
